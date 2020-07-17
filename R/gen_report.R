002e82b1f5493f2bd1fe84b4a0bfcaf291bef3f5
fluentin44 <fluentin44languages@gmail.com>
2020-07-17 13:19:36 +0100

    Changed vovariate to covariate names

--- R/gen_report.R
+++ R/gen_report.R
@@ -11,7 +11,7 @@
 #' @examples
 #'
 #' Need to specify the raw results path, the report file *without* extenstion and output file name *without* extension
-gen_report <- function(raw_results_path, report_template_filename, output_file_name, covariates){
+gen_report <- function(raw_results_path, report_template_filename, output_file_name, covariate_names){
   report_path <- paste0("./reports/", report_template_filename, ".Rmd")
   output_path <- "./reports/outs/"
   rmarkdown::render(
@@ -20,7 +20,7 @@ gen_report <- function(raw_results_path, report_template_filename, output_file_n
     output_file = paste0(output_file_name, ".html"),
     params = list(name = str_replace(output_file_name, ".html", ""),
                   raw_results = raw_results_path,
-                  covariates = covariates
+                  covariates = covariate_names
     )
   )
 }
