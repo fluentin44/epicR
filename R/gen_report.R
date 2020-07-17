b224fddaa3466d4bf30e8eecd6775c581fb38d67
fluentin44 <fluentin44languages@gmail.com>
2020-07-17 13:13:56 +0100

    documented functions

--- R/gen_report.R
+++ R/gen_report.R
@@ -3,6 +3,7 @@
 #' @param output_file_name
 #' @param raw_results_path
 #' @param report_template_filename
+#' @param covariates
 #'
 #' @return Rmarkdown report in an outs folder in reports folder
 #' @export
@@ -10,7 +11,7 @@
 #' @examples
 #'
 #' Need to specify the raw results path, the report file *without* extenstion and output file name *without* extension
-gen_report <- function(raw_results_path, report_template_filename, output_file_name){
+gen_report <- function(raw_results_path, report_template_filename, output_file_name, covariates){
   report_path <- paste0("./reports/", report_template_filename, ".Rmd")
   output_path <- "./reports/outs/"
   rmarkdown::render(
@@ -19,7 +20,7 @@ gen_report <- function(raw_results_path, report_template_filename, output_file_n
     output_file = paste0(output_file_name, ".html"),
     params = list(name = str_replace(output_file_name, ".html", ""),
                   raw_results = raw_results_path,
-                  covariates = covariates,
+                  covariates = covariates
     )
   )
 }
