7729375ff8d86cb431ba8204c1b461262744e50e
Matthew Hewitt <fluentin44@Matthews-MacBook-Pro.local>
2022-06-09 17:06:54 +0100

    Modified normality graphs

--- R/normality_graphs.R
+++ R/normality_graphs.R
@@ -7,18 +7,18 @@
 #' @export
 #'
 #' @examples
-normality_graphs <- function(df, variable){
-
-  df <- df[!is.na(df[[variable]]), ]
-  p1 <- ggdensity(df[[variable]], fill = "lightgray") +
+bip <- function (df, variable) {
+  # df <- df[!is.na(df[[name]]), ]
+  #var <- sym(variable)
+  p1 <- ggdensity(df, variable,
+                  fill = "lightgray",
+                  xlab = variable) +
     stat_overlay_normal_density(color = "red", linetype = "dashed")
-  p2 <- ggplot(df, aes(x = df[[variable]])) +
-    geom_histogram(color = "darkblue", fill = "lightblue")
-  p3 <- ggqqplot(df[[variable]])
-  p4 <- ggplot(df, aes(x = "", y = df[[variable]])) +
-    geom_boxplot(outlier.colour = "red",
-                 outlier.size = 2)
-  #print(paste0("Number of values for ", variable, " = ", length(df[[variable]])))
+  p2 <- df %>% ggplot(aes(x = pull(., variable))) + geom_histogram(color = "darkblue", fill = "lightblue") +
+    xlab(variable)
+  p3 <- ggqqplot(df, variable)
+  p4 <- df %>% ggplot(aes(x = "", y = pull(., variable))) +
+    geom_boxplot(outlier.colour = "red", outlier.size = 2) +
+    ylab(variable)
   grid.arrange(p1, p2, p3, p4, ncol = 2)
-  #grid.arrange(p2, p4, ncol = 2)
 }
